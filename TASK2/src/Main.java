1ac0a6003b093f8d698ae3951a4eb00667686dc1
Esraa-Megahed <esnoad123@yahoo.com>
2023-12-23 13:59:44 +0200

    changed

--- TASK2/src/Main.java
+++ TASK2/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!\nhello rosita");
+        System.out.println("Hello world!\nhello Esraagit ");
     }
 }
